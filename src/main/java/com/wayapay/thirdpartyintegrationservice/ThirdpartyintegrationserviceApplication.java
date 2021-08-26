dd766e00854cb9d48c79baf71e33ac6f25f71d7f
agbeterseer <agbe.terseer@gmail.com>
2021-08-26 10:50:59 +0100

    remove hereoas

--- src/main/java/com/wayapay/thirdpartyintegrationservice/ThirdpartyintegrationserviceApplication.java
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/ThirdpartyintegrationserviceApplication.java
@@ -8,9 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @EnableJpaAuditing
 @SpringBootApplication
 public class ThirdpartyintegrationserviceApplication {

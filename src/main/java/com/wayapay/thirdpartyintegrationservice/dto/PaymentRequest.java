d34f0951b0d79dfdc5771144415e129c8f413c30
agbeterseer <agbe.terseer@gmail.com>
2021-08-26 05:06:10 +0100

    updates on all controllers

--- src/main/java/com/wayapay/thirdpartyintegrationservice/dto/PaymentRequest.java
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/dto/PaymentRequest.java
@@ -1,5 +1,6 @@
 package com.wayapay.thirdpartyintegrationservice.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,20 +20,41 @@ import java.util.List;
 @ToString
 public class PaymentRequest {
 
+    @ApiModelProperty(example = "Airtime")
     @NotBlank(message = "categoryId is required")
     private String categoryId;
 
+    @ApiModelProperty(example = "mtnvtu")
     @NotBlank(message = "billerId is required")
     private String billerId;
 
+    @ApiModelProperty(example = "79")
     @NotBlank(message = "source wallet account number is required")
-    //todo Ensure that it is only numbers
     private String sourceWalletAccountNumber;
 
+    @ApiModelProperty(example = "50")
     @NotNull(message = "amount is required")
     @DecimalMin(value = "0.01", message = "minimum amount required is 0.01")
     private BigDecimal amount;
 
+    @ApiModelProperty(example = " [\n" +
+            "    {\n" +
+            "      \"name\": \"phone\",\n" +
+            "      \"value\": \"+2348189747520\"\n" +
+            "    },\n" +
+            "\t{\n" +
+            "      \"name\": \"amount\",\n" +
+            "      \"value\": \"50\"\n" +
+            "    },\n" +
+            "\t{\n" +
+            "      \"name\": \"paymentMethod\",\n" +
+            "      \"value\": \"cash\"\n" +
+            "    },\n" +
+            "\t{\n" +
+            "      \"name\": \"channel\",\n" +
+            "      \"value\": \"ATM\"\n" +
+            "    }\n" +
+            "  ]")
     @NotEmpty(message = "No item provided, AtLeast provide an item for validation")
     private List<ParamNameValue> data = new ArrayList<>();
 

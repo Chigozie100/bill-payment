a4b2de79df935d8872399bc4f98e00ad7b0fc507
agbeterseer <agbe.terseer@gmail.com>
2021-09-10 15:27:20 +0100

    tested select

---
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/service/AdminService.java
@@ -0,0 +1,10 @@
+package com.wayapay.thirdpartyintegrationservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+public class AdminService {
+
+}

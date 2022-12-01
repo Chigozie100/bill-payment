787742653ad5889d5dbfe900c3345cf624628d51
Oseni Oluwasina <oluwasina.oseni@dynamowit.com>
2022-12-01 09:20:14 +0100

    fix: betting

--- src/main/java/com/wayapay/thirdpartyintegrationservice/service/baxi/BaxiService.java
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/service/baxi/BaxiService.java
@@ -121,6 +121,8 @@ public class BaxiService implements IThirdPartyService {
                 return getElectricityPaymentItems(categoryId, billerId);
             case EPIN:
                 return getEPinBundlePaymentItems(categoryId, billerId);
+            case BETTING:
+                return getEPinBundlePaymentItems(categoryId, billerId);
             default:
                 throw new ThirdPartyIntegrationException(HttpStatus.BAD_REQUEST, "Invalid CategoryId provided");
         }

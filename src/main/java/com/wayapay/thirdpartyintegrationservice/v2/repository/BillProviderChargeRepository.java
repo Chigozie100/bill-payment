428bc33e7028f86262b3b0937ea340883c4ddc3c
Bolaji Oyewumi <49565233+omabolaji@users.noreply.github.com>
2023-07-10 19:29:46 +0100

    update on service charges filter by providers

--- src/main/java/com/wayapay/thirdpartyintegrationservice/v2/repository/BillProviderChargeRepository.java
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/v2/repository/BillProviderChargeRepository.java
@@ -3,6 +3,8 @@ package com.wayapay.thirdpartyintegrationservice.v2.repository;
 import com.wayapay.thirdpartyintegrationservice.v2.entity.BillProviderCharge;
 import com.wayapay.thirdpartyintegrationservice.v2.entity.ServiceProvider;
 import com.wayapay.thirdpartyintegrationservice.v2.entity.ServiceProviderCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,4 +15,8 @@ public interface BillProviderChargeRepository extends JpaRepository<BillProvider
     Optional<BillProviderCharge> findByServiceProviderAndServiceProviderCategoryAndIsActiveAndIsDeleted(ServiceProvider serviceProvider, ServiceProviderCategory category, boolean isActive, boolean isDeleted);
 
     Optional<BillProviderCharge> findByIdAndIsActiveAndIsDeleted(Long id, boolean isActive, boolean isDeleted);
+
+    Page<BillProviderCharge> findAllByIsActiveAndIsDeleted(boolean isActive, boolean isDeleted, Pageable pageable);
+
+    Page<BillProviderCharge> findAllByServiceProviderAndIsActiveAndIsDeleted(ServiceProvider serviceProvider, boolean isActive, boolean isDeleted, Pageable pageable);
 }

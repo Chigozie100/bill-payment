84ed34143cbe643d6905a4cd5a1e05ae7cdac6f7
Edceebee <edceebee@gmail.com>
2024-02-28 13:51:19 +0100

    refactored interceptor

--- src/main/java/com/wayapay/thirdpartyintegrationservice/util/Constants.java
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/util/Constants.java
@@ -13,6 +13,7 @@ public class Constants {
 
     public static final String TOKEN_PREFIX = "serial ";
     public static final String HEADER_STRING = "Authorization";
+
     public static final String AUTHORITIES_KEY = "scopes";
     public static final String USERNAME = "username";
     public static final String ROLE = "role";

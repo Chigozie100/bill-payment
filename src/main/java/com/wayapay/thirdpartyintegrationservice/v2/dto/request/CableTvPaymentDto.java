45204d34de7bf08e9d20b3f002152351ed17c6c8
chigozie <chigozieprincenwigwe@gmail.com>
2024-11-20 11:23:49 +0000

    cableTv refactor

--- src/main/java/com/wayapay/thirdpartyintegrationservice/v2/dto/request/CableTvPaymentDto.java
+++ src/main/java/com/wayapay/thirdpartyintegrationservice/v2/dto/request/CableTvPaymentDto.java
@@ -3,10 +3,14 @@ package com.wayapay.thirdpartyintegrationservice.v2.dto.request;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data @JsonIgnoreProperties(ignoreUnknown = true)
 public class CableTvPaymentDto {
+    @NotNull(message = "service type must not be null")
     private String type;
     private String phone;
+    @NotNull(message = "amount must not be null")
     private String amount;
     private String email;
     private String productCode;
